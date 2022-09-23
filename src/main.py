f1ccde1835c0683415fbbc58aab6aef7ac6c4b0e
Lucas Capellini <lucascapellini00@gmail.com>
2022-09-22 23:48:56 -0300

    Correção de bug com primeiros caracteres repetidos e melhor formatação da resposta

--- src/main.py
+++ src/main.py
@@ -37,17 +37,12 @@ def createTransitions(prevState, events, nextState):
 def otherEvents(events):
     eventsCopy = ["A", "C", "G", "T"]
     for event in ["A", "C", "G", "T"]:
-        print(events, event, eventsCopy)
         if(event in events):
             eventsCopy.remove(event)
     return eventsCopy
     
-
-def createDnaSearcherAutomaton(sequence):
-    automatonTransitions = []
-
+def createStatesSequence(sequence):
     firstState = state("0", marked=False)
-    # lastState = state(str(len(sequence)), marked=True)
     allStates = []
 
     allStates.append(firstState)
@@ -58,34 +53,88 @@ def createDnaSearcherAutomaton(sequence):
             allStates.append(state(str(index), marked=True))
         else:
             allStates.append(state(str(index), marked=False))
+            
+    return allStates
+
+def createDnaSearcherAutomaton(sequence):
+    automatonTransitions = []
+
+    allStates = createStatesSequence(sequence)
 
     
-    # print(len(allStates))
+    dnasList = list(sequence)
+    repeatsFirstPosition = 0
     
-    for index in range(len(sequence)):
-        currentState = allStates[index]
-        nextState = allStates[index + 1]
+    for index in range(len(dnasList)):
+        if(dnasList[index] == dnasList[index + 1]):
+            
+            currentState = allStates[index]
+            nextState = allStates[index + 1]
+            
+            currentEvent = getEvent(sequence[index])
+            transition = getTransitionData(currentState, currentEvent, nextState)
+            automatonTransitions.append(transition)
+            
+            automatonTransitions.extend(createTransitions(currentState, otherEvents([sequence[index], sequence[0]]), allStates[0]))
+            # print("automatonTransitions", automatonTransitions)
         
-        currentEvent = getEvent(sequence[index])
+            # dnasList.remove(dnasList[0])
+            
+            repeatsFirstPosition += 1
+        else:
+            break
+    
+    formattedSequence = "".join(dnasList)
+    print("formattedSequence and dnasList", formattedSequence, dnasList)
+    
+    for index in range(len(formattedSequence) - repeatsFirstPosition):
+        offSetIndex = index + repeatsFirstPosition
+        print("offsetIndex", offSetIndex, repeatsFirstPosition, formattedSequence[offSetIndex])
+        currentState = allStates[offSetIndex]
+        nextState = allStates[offSetIndex + 1]
+        
+        currentEvent = getEvent(formattedSequence[offSetIndex])
         transition = getTransitionData(currentState, currentEvent, nextState)
         automatonTransitions.append(transition)
         
-        print(currentState, nextState, index)
-        
-
-        if(index == 1):
-            automatonTransitions.extend(createTransitions(currentState, otherEvents([sequence[1], sequence[0]]), allStates[index - 1]))
-            automatonTransitions.append(getTransitionData(currentState, getEvent(sequence[0]), currentState))
-        else:
-            automatonTransitions.extend(createTransitions(currentState, otherEvents([sequence[index], sequence[0]]), allStates[0]))
-            automatonTransitions.append(getTransitionData(currentState, getEvent(sequence[0]), allStates[1]))
+        # print(currentState, nextState, offSetIndex)
             
-    automatonTransitions.extend(createTransitions(allStates[len(allStates) - 1], otherEvents([sequence[0], sequence[len(sequence) - 1]]), allStates[0]))
+
+        if(offSetIndex == repeatsFirstPosition + 1):
+            automatonTransitions.extend(createTransitions(currentState, otherEvents([formattedSequence[repeatsFirstPosition + 1], formattedSequence[repeatsFirstPosition]]), allStates[offSetIndex - 1]))
+            automatonTransitions.append(getTransitionData(currentState, getEvent(formattedSequence[repeatsFirstPosition]), currentState))
+        else:
+            automatonTransitions.extend(createTransitions(currentState, otherEvents([formattedSequence[offSetIndex], formattedSequence[repeatsFirstPosition]]), allStates[repeatsFirstPosition]))
+            automatonTransitions.append(getTransitionData(currentState, getEvent(formattedSequence[repeatsFirstPosition]), allStates[repeatsFirstPosition + 1]))
+        
+        # print("automatonTransitions", automatonTransitions)
+            
+    automatonTransitions.extend(createTransitions(allStates[len(allStates) - 1], otherEvents([formattedSequence[0], formattedSequence[len(formattedSequence) - 1]]), allStates[0]))
+    automatonTransitions.append(getTransitionData(allStates[len(allStates) - 1], getEvent(formattedSequence[0]), allStates[1]))
+    
+    
 
 
-    G1 = dfa(automatonTransitions, firstState, "G1")
+    G1 = dfa(automatonTransitions, allStates[0], "G1")
 
     return G1
+
+def createDNASequence(roleSequence):
+    
+    allStates = createStatesSequence(roleSequence)
+    automatonTransitions = []
+    
+    for index in range(len(roleSequence)):
+        currentState = allStates[index]
+        nextState = allStates[index + 1]
+        
+        currentEvent = getEvent(roleSequence[index])
+        transition = getTransitionData(currentState, currentEvent, nextState)
+        automatonTransitions.append(transition)
+        
+    G2 = dfa(automatonTransitions, allStates[0], "G2")
+     
+    return G2
 
 def formatSequence(originalSequence: str):
     dnasList = list(originalSequence)
@@ -104,16 +153,45 @@ def dnaSample(length):
 
 # dnaForTest = dnaSample(4)
 
-dnaForTest = "AACCGATTCCGA"
+dnaToFind = "AACCT"
+dna = "AAACCT" + dnaSample(5555) + "GAAACCTG"
   
-print(dnaForTest)
+print(dnaToFind, dna)
 
-formatedDNASequence = formatSequence(dnaForTest)
+# formatedDNASequence = formatSequence(dnaToFind)
 
-print(formatedDNASequence)
-g = createDnaSearcherAutomaton(formatedDNASequence)
+g1 = createDnaSearcherAutomaton(dnaToFind)
+g2 = createDNASequence(dna)
 
+gp = parallel_composition(g1,g2)
 
-automatonImage = show_automaton(g)
+# print(gp)
 
-print(automatonImage.data)
+def findMatch(automataResult: str):
+    print("autromatonResult", automataResult)
+    positinToBeginSearch = automataResult.find("node [shape = circle];")
+    positionToFinishSearch = automataResult.find("node [shape = point ]")
+    # print("splitResult", automataResult[positinToBeginSearch:positionToFinishSearch])
+    filteredAutomataResult = automataResult[positinToBeginSearch:(positionToFinishSearch + 22)]
+    cleanResult = filteredAutomataResult.split(" ")
+    # print("cleanResult", cleanResult)
+    
+    statesWithDna = []
+    for state in cleanResult:
+        # print(state)
+        
+        positionOfSlash = state.find("|")
+        lastNumberCharacter = len(state) - 1
+        
+        if(len(state) > 1 and state[1].isnumeric() and int(state[1]) == (len(dnaToFind))):
+            statesWithDna.append(state[positionOfSlash + 1:lastNumberCharacter])
+            # print("CHEGOUUU", state, statesWithDna[1:])
+            
+    return statesWithDna
+    
+        
+
+automatonImage = show_automaton(gp)
+print("Posições encontradas", findMatch(automatonImage.data))
+
+# print(automatonImage.data)
